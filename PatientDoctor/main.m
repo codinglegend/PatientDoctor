c945c50a334ed7984e6cb104789a42dddc433485
Alain K <alain.kahwaji@gmail.com>
2015-05-07 10:40:58 -0700

    Initial Commit

---
+++ PatientDoctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PatientDoctor
+//
+//  Created by Alain  on 2015-05-07.
+//  Copyright (c) 2015 Alain . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
